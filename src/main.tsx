8f61a7b23dd80bd565f78133e897ed579c158fe5
fuadz26 <ahmadfuadi2611@gmail.com>
2026-02-22 12:54:51 +0700

    noted

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import AdminAduhai from "./adminAduhai";
+import AdminAduhai from "./admi";
 
 
 createRoot(document.getElementById("root")!).render(
